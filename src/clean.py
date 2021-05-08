bcb4e35bf97fae136eebdebbac9851a04191bce0
binoychitale <binoychitale@gmail.com>
2021-05-08 10:58:35 +0530

    Generic outlier detection for all columns

--- src/clean.py
+++ src/clean.py
@@ -3,20 +3,21 @@ import math
 
 def get_cleaned_data(filename):
     data_raw = pandas.read_csv(filename)
-    deleted_rows = {}
-    CT_confirmed = data_raw.sort_values(by="CT confirmed", ascending=False)["CT confirmed"]
-    Q3_index = math.ceil(75/100 * len(CT_confirmed))
-    Q1_index = math.ceil(25/100 * len(CT_confirmed))
-    alpha = 1.5
-    IQR = CT_confirmed[Q3_index] - CT_confirmed[Q1_index]
 
-    upper_threshold = CT_confirmed[Q3_index] + alpha * IQR
-    lower_threshold = CT_confirmed[Q1_index] - alpha * IQR
+    for column in data_raw:
+        if column == 'Date':
+            continue
+        column_data = data_raw.sort_values(by=column)[column]
+        Q3_index = math.ceil(75/100 * len(column_data))
+        Q1_index = math.ceil(25/100 * len(column_data))
+        alpha = 1.5
+        IQR = column_data[Q3_index] - column_data[Q1_index]
+        upper_threshold = column_data[Q3_index] + alpha * IQR
+        lower_threshold = column_data[Q1_index] - alpha * IQR
 
-    outliers = data_raw[(data_raw['CT confirmed'] != 0) & ((data_raw['CT confirmed'] > upper_threshold) | (data_raw['CT confirmed'] < lower_threshold))].index
+        outliers = data_raw[(data_raw[column] != 0) & ((data_raw[column] > upper_threshold) | (data_raw[column] < lower_threshold))].index
 
-    data_raw.drop(outliers, inplace=True)
-
-    print ("Dropped %s outlier rows \nIQR = %s \nlower thresold = %s \nupper thresold = %s" % (len(outliers), IQR, lower_threshold, upper_threshold))
+        data_raw.drop(outliers, inplace=True)
+        print ("\nColumn: %s \nDropped %s outlier rows \nIQR = %s \nlower thresold = %s \nupper thresold = %s" % (column, len(outliers), IQR, lower_threshold, upper_threshold))
 
     return data_raw
