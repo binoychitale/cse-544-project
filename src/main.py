d2db51230e1f12b23ff476aa53272897ebcbfa72
Akshay Somayaji <akshayafc1995@gmail.com>
2021-05-10 11:27:35 +0530

    KS 2-Sample Test (2c)

--- src/main.py
+++ src/main.py
@@ -1,4 +1,9 @@
 import clean
+from ks_test import KS_2_Sample_Test
 
 data = clean.get_cleaned_data("../data/States Data/4.csv")
 #print(data)
+
+# 2c) Perform 2-Sample KS Test on the #cases/#deaths data of the 2 states
+KS_2_Sample_Test(data, 'confirmed')
+KS_2_Sample_Test(data, 'deaths')
