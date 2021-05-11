0fd360d1c4f5a896847fef83787bd64ca9f125d7
Akshay Somayaji <akshayafc1995@gmail.com>
2021-05-11 19:53:53 +0530

    Bayesian Inference for lambda (2d)

--- src/main.py
+++ src/main.py
@@ -1,4 +1,8 @@
 import clean
+from posterior import calculate_posterior
 
 data, daily_data = clean.get_cleaned_data("../data/States Data/4.csv")
 #print(data)
+
+# 2d) Apply Bayesian Inference to calculate the posterior for combined deaths data
+calculate_posterior(daily_data)
