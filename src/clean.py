28bdaf24b6d0bfe1f7b3ef6df72c62846571149d
binoychitale <binoychitale@gmail.com>
2021-05-11 08:44:04 +0530

    Fix data-cleaning to first extract daily data before applying Tukeys rule

--- src/clean.py
+++ src/clean.py
@@ -1,5 +1,7 @@
 import pandas
 import math
+import numpy as np
+
 
 def get_cleaned_data(filename):
     data_raw = pandas.read_csv(filename)
@@ -7,7 +9,9 @@ def get_cleaned_data(filename):
     for column in data_raw:
         if column == 'Date':
             continue
-        sorted_column_data = data_raw.sort_values(by=column)[column]
+        daily_data = data_raw[column].diff()
+        sorted_column_data = np.array(daily_data.sort_values())
+
         Q3_index = math.ceil(75/100 * len(sorted_column_data))
         Q1_index = math.ceil(25/100 * len(sorted_column_data))
         alpha = 1.5
@@ -15,9 +19,8 @@ def get_cleaned_data(filename):
         upper_threshold = sorted_column_data[Q3_index] + alpha * IQR
         lower_threshold = sorted_column_data[Q1_index] - alpha * IQR
 
-        outliers = data_raw[(data_raw[column] != 0) & ((data_raw[column] > upper_threshold) | (data_raw[column] < lower_threshold))].index
+        outliers = daily_data[(daily_data != 0) & ((daily_data < lower_threshold) | (daily_data > upper_threshold))].index
 
         data_raw.drop(outliers, inplace=True)
         print ("\nColumn: %s \nDropped %s outlier rows \nIQR = %s \nlower thresold = %s \nupper thresold = %s" % (column, len(outliers), IQR, lower_threshold, upper_threshold))
-
     return data_raw
