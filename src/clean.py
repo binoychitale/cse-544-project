213f7db5fdcc2d1937a594216450b966d483ca11
binoychitale <binoychitale@gmail.com>
2021-05-11 11:28:24 +0530

    Bugfix: outlier detection, expose daily data API

--- src/clean.py
+++ src/clean.py
@@ -5,6 +5,7 @@ import numpy as np
 
 def get_cleaned_data(filename):
     data_raw = pandas.read_csv(filename)
+    outliers = set()
 
     for column in data_raw:
         if column == 'Date':
@@ -19,8 +20,20 @@ def get_cleaned_data(filename):
         upper_threshold = sorted_column_data[Q3_index] + alpha * IQR
         lower_threshold = sorted_column_data[Q1_index] - alpha * IQR
 
-        outliers = daily_data[(daily_data != 0) & ((daily_data < lower_threshold) | (daily_data > upper_threshold))].index
+        column_outliers = daily_data[(daily_data != 0) & ((daily_data < lower_threshold) | (daily_data > upper_threshold))].index
+        outliers = outliers.union(column_outliers)
+        print ("\nColumn: %s \nDropped %s outlier rows \nIQR = %s \nlower thresold = %s \nupper thresold = %s" % (
+        column, len(column_outliers), IQR, lower_threshold, upper_threshold))
 
-        data_raw.drop(outliers, inplace=True)
-        print ("\nColumn: %s \nDropped %s outlier rows \nIQR = %s \nlower thresold = %s \nupper thresold = %s" % (column, len(outliers), IQR, lower_threshold, upper_threshold))
+    data_raw.drop(outliers, inplace=True)
+
     return data_raw
+
+
+def get_daily_data(cumulative_data):
+    for column in cumulative_data:
+        if column == 'Date':
+            continue
+        cumulative_data[column] = cumulative_data[column].diff()
+
+    return cumulative_data
