95639418d474fd4b746f898b9aa8f3d6e53dfb05
Akshay Somayaji <akshayafc1995@gmail.com>
2021-05-12 21:42:10 +0530

    Wald's, Z Tests (2b)

--- src/main.py
+++ src/main.py
@@ -1,4 +1,8 @@
 import clean
+from hypothesis_tests import run_hypothesis_tests
 
 data, daily_data = clean.get_cleaned_data("../data/States Data/4.csv")
 #print(data)
+
+# 2b) Wald's, Z and T Tests on the #cases/#deaths data of the 2 states in the given time range
+run_hypothesis_tests(daily_data)
